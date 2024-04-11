aec6bea929ae4d5fb8d40ea8ddd32aa165c9fb68
mnachevaw <mnacheva1881@gmail.com>
2024-04-11 19:19:40 +0300

    test

---
+++ lab9/simple_data_types.py
@@ -0,0 +1,12 @@
+# expression: 1 + 1
+print('1'+ '1')
+print(1 + 1)
+print(2+3)
+print(2-3)
+print(2*3)
+print(2/3)
+print(2//3)
+print(7//2) # modulus(reminder)
+import math
+print(math.ceil(2,11))
+print(math.floor(2.11))
